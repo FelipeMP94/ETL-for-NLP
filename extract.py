7969cd619feee304f32ecd5a0a22e8dcc27525ff
FelipeMP94 <felipe00depaula@gmail.com>
2024-01-30 10:53:37 -0300

    Classe extractor

--- extract.py
+++ extract.py
@@ -4,60 +4,58 @@ import time
 from selenium.webdriver.common.by import By
 from urls.urls import infinitscroll
 
-# Inicializa o WebDriver do Chrome
-def extract_links():
-    driver = webdriver.Chrome()
-
-    # Abre a página desejada
-    driver.get(infinitscroll)
-
-    # Define um tempo de espera entre as rolagens
-    SCROLL_PAUSE_TIME = 2
-
-    # Obter a altura inicial da página
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    count = 0 
-    while True:
-        # Rola até o final da página
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Aguarda o carregamento da página
-        time.sleep(SCROLL_PAUSE_TIME)
-
-        # Calcula a nova altura da página e compara com a última altura da página
-        new_height = driver.execute_script("return document.body.scrollHeight")
-    
-        if (new_height == last_height):
-            count+=1
-            if count ==3:
-                print('Fim')
-                break
-        else:
-            count = 0
-
-        last_height = new_height
-    
-    # Aqui você pode adicionar o código para extrair os dados necessários da página
-    elems = driver.find_elements(By.XPATH,'//a[@class="gh-card-link"]')
-    cont = 0
-    links = [] 
-    for elem in elems:
-        links.append(elem.get_attribute("href"))
-
-    # Fecha o navegador quando terminar
-    arq = open('Dados/links.txt','w')
-    for link in links:
-        arq.write('\n'+link)
-    driver.quit()
 
 
-def prepare_links():
-    arq = open('Dados\links.txt')
-    links = []
-    for line in arq.readlines():
-        line = line.replace('\n','')
-        links.append(line)
-    links.pop(0)
-    return links
+class Extractor:
+    def __init__(self,):
+        self.path_principal = infinitscroll
+        self.driver = webdriver.Chrome()
+        self.SCROLL_PAUSE_TIME = 2
 
+
+    def extract_links_class(self):
+        self.driver.get(self.path_principal)
+
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        count = 0 
+        while True:
+            # Rola até o final da página
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Aguarda o carregamento da página
+            time.sleep(self.SCROLL_PAUSE_TIME)
+
+            # Calcula a nova altura da página e compara com a última altura da página
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+        
+            if (new_height == last_height):
+                count+=1
+                if count ==3:
+                    break
+            else:
+                count = 0
+
+            last_height = new_height
+         # Aqui você pode adicionar o código para extrair os dados necessários da página
+        elems = self.driver.find_elements(By.XPATH,'//a[@class="gh-card-link"]')
+        links = [] 
+        for elem in elems:
+            links.append(elem.get_attribute("href"))
+
+        # Fecha o navegador quando terminar
+        arq = open('Dados/linksTest.txt','w')
+        for link in links:
+            arq.write('\n'+link)
+        self.driver.quit()
+
+    def prepare_links(self):
+        arq = open('Dados\linksTest.txt')
+        links = []
+        for line in arq.readlines():
+            line = line.replace('\n','')
+            links.append(line)
+        links.pop(0)
+        return links
+
+#
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from extract import extract_links, prepare_links
+from extract import Extractor, prepare_links
 from transform import create_objects
 from load import save_jason
 
 
 if __name__ == '__main__':
-   links = prepare_links()
-   obj = create_objects(links)
-   print(f'Quantidade de tribunas enviadas: {len(obj)}')
-   save_jason(obj)
+    extractor = Extractor()
+    extractor.extract_links_class()
+    links = extractor.prepare_links()
+    print(links)
+   
    
  
